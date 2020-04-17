5e968c5265ad01fd7262bb96910568b40666b82b
Adam <adamhursey24@gmail.com>
2020-04-17 16:40:13 -0600

    added background image

---
+++ src/components/MainHeader/MainHeader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MainHeader = () => {
+  return (
+    <div>
+      <img
+        className="headerImage"
+        src="/images/northland-dev-landing-page.jpg"
+      ></img>
+      <h1 className="title">
+        <span className="Northland">Northland</span>
+        <span className="Development">Development</span>
+      </h1>
+    </div>
+  );
+};
+
+export default MainHeader;
